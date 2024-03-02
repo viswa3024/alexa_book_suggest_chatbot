344073273f2ba946508bd54d53bd7b79cb4aaa3f
Recorichardretardo <hi@hi.com>
2024-03-02 17:28:12 +0530

    first commit

---
+++ lambda/lambda_function.py
@@ -0,0 +1,283 @@
+# -*- coding: utf-8 -*-
+
+# This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK for Python.
+# Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+# session persistence, api calls, and more.
+# This sample is built using the handler classes approach in skill builder.
+import logging
+import ask_sdk_core.utils as ask_utils
+
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.handler_input import HandlerInput
+
+from ask_sdk_model import Response
+
+import pandas as pd
+import requests
+import io
+import calendar
+import random
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class LaunchRequestHandler(AbstractRequestHandler):
+    """Handler for Skill Launch."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+
+        return ask_utils.is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "Hello! Welcome to Hackathon project. Do you want know your Zodiac Signs or want me to suggest a book?"
+        #reprompt_text = "I was born Nov.6th,2014. When you were born?"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
+
+
+class CaptureZodiacSignIntentHandler(AbstractRequestHandler):
+    """Handler for Hello World Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("CaptureZodiacSignIntent")(handler_input)
+        
+    def filter(self, X):
+        date = X.split()
+        month = date[0]
+        month_as_index = list(calendar.month_abbr).index(month[:3].title())
+        day = int(date[1])
+        return (month_as_index,day)
+
+    def handle(self, handler_input):
+        slots = handler_input.request_envelope.request.intent.slots
+        year = slots["year"].value
+        month = slots["month"].value
+        day = slots["day"].value
+
+        url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSe2IkvCKagdC5ARC_p_BW95_vKs8O2I0Emh_EMilrX_elyAlF60tsx_4kTaOabvw/pub?gid=1283308611&single=true&output=csv"
+        csv_content = requests.get(url).content
+        df = pd.read_csv(io.StringIO(csv_content.decode('utf-8')))
+        zodiac= ""
+        month_as_index = list(calendar.month_abbr).index(month[:3].title())
+        usr_dob = (month_as_index, int(day))
+        text = df['Zodiac'].iloc[0]
+        for index, row in df.iterrows():
+            if self.filter(row['Start']) <= usr_dob <= self.filter(row["End"]):
+                zodiac = row['Zodiac']
+        #speak_output = text
+        speak_output = "I see you were born on the {day} of {month} {year},  {zodiac}.".format(month=month,day=day,year=year,zodiac=zodiac)
+        
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
+class SuggestBookIntentHandler(AbstractRequestHandler):
+    """Handler for Hello World Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("SuggestBookIntent")(handler_input)
+
+    def get_books_list(self, df):
+        books = []
+        for _, row in df.iterrows():
+            book = {
+                'Title': row['Title'],
+                'Author': row['Author'].lower(),
+                'Language': row['Language'].lower(),
+                'Genre': row['Genre'].lower()
+            }
+            books.append(book)
+        return books
+
+    def recommend_book(self, preferences, books):
+        filtered_books = [book for book in books if all(book[key] == value for key, value in preferences.items())]
+
+        if filtered_books:
+            return random.choice(filtered_books)
+        else:
+            return "No matching books found."
+
+    def handle(self, handler_input):
+        slots = handler_input.request_envelope.request.intent.slots
+        
+        if slots["author"].value:
+            author = slots["author"].value.lower()
+        else:
+            author = None
+        if slots["language"].value:
+            language = slots["language"].value.lower()
+        else:
+            language = None
+        if slots["genre"].value:
+            genre = slots["genre"].value.lower()
+        else:
+            genre = None
+        
+        user_preferences = {}
+        if author and author != 'author':
+            user_preferences["Author"] = author
+        if language and language != 'language':
+            user_preferences["Language"] = language
+        if genre and genre != 'genre':
+            user_preferences["Genre"] = genre
+            
+
+        url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRk7Qv4QiZGx7qYlzwHWNObUZGppcht-SdETM1UgISaetm0Y0ziEDjMDGHPz_kpYA/pub?gid=353356446&single=true&output=csv"
+        csv_content = requests.get(url).content
+        df = pd.read_csv(io.StringIO(csv_content.decode('utf-8')))
+        books_list = self.get_books_list(df)
+        recommended_book = self.recommend_book(user_preferences,books_list)
+
+        if isinstance(recommended_book, dict):
+            author_name = recommended_book['Author'].title()
+            speak_output = f"Recommended Book: {recommended_book['Title']} by {author_name}"
+        else:
+            speak_output = "author is  {author}, language is {language}, genre is {genre}, No matching books found.".format(author=author,language=language,genre=genre)
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
+
+
+class HelpIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "You can say hello to me! How can I help?"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
+
+
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    """Single handler for Cancel and Stop Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "Goodbye!"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .response
+        )
+
+class FallbackIntentHandler(AbstractRequestHandler):
+    """Single handler for Fallback Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("AMAZON.FallbackIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        logger.info("In FallbackIntentHandler")
+        speech = "Hmm, I'm not sure. You can say Hello or Help. What would you like to do?"
+        reprompt = "I didn't catch that. What can I help you with?"
+
+        return handler_input.response_builder.speak(speech).ask(reprompt).response
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    """Handler for Session End."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+
+        # Any cleanup logic goes here.
+
+        return handler_input.response_builder.response
+
+
+class IntentReflectorHandler(AbstractRequestHandler):
+    """The intent reflector is used for interaction model testing and debugging.
+    It will simply repeat the intent the user said. You can create custom handlers
+    for your intents by defining them above, then also adding them to the request
+    handler chain below.
+    """
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_request_type("IntentRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        intent_name = ask_utils.get_intent_name(handler_input)
+        speak_output = "You just triggered " + intent_name + "."
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                # .ask("add a reprompt if you want to keep the session open for the user to respond")
+                .response
+        )
+
+
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    """Generic error handling to capture any syntax or routing errors. If you receive an error
+    stating the request handler chain is not found, you have not implemented a handler for
+    the intent being invoked or included it in the skill builder below.
+    """
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.error(exception, exc_info=True)
+
+        speak_output = "Sorry, I had trouble doing what you asked. Please try again."
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
+
+# The SkillBuilder object acts as the entry point for your skill, routing all request and response
+# payloads to the handlers above. Make sure any new handlers or interceptors you've
+# defined are included below. The order matters - they're processed top to bottom.
+
+
+sb = SkillBuilder()
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(CaptureZodiacSignIntentHandler())
+sb.add_request_handler(SuggestBookIntentHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(FallbackIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+
+sb.add_exception_handler(CatchAllExceptionHandler())
+
+lambda_handler = sb.lambda_handler()
